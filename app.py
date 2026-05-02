7d0fd98d509aa6217b973e4ac4443ed874628d42
vladislavgumenuk <vlad.os.progr@gmail.com>
2026-05-02 13:31:35 +0300

    [FIX]

--- app.py
+++ app.py
@@ -5,4 +5,9 @@ def add(a: int, b: int) -> int:
 def divide(a: float, b: float) -> float:
     if b == 0:
         raise ValueError("Division by zero")
+
     return a / b
+
+
+if __name__ == "__main__":
+    print("Application is running")
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,4 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+import pytest
 
 from app import add, divide
 
