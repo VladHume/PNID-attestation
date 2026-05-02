ae22687aa37ddbc8ff6b682902d1dd6e3232a929
vladislavgumenuk <vlad.os.progr@gmail.com>
2026-05-02 13:36:49 +0300

    [FIX] app.py & test_app.py to ci.yml

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 def add(a: int, b: int) -> int:
     return a + b
 
+
 def divide(a: float, b: float) -> float:
     if b == 0:
         raise ValueError("Division by zero")
 
     return a / b
+
 
 if __name__ == "__main__":
     print("Application is running")
